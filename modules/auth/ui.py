880e3de52118eaaf67ce6f242e4e6956c51e2094
mirux <myrux14@gmail.com>
2026-06-01 21:30:49 -0500

    cambios

--- modules/auth/ui.py
+++ modules/auth/ui.py
@@ -29,6 +29,19 @@ def logout():
 # =========================================
 def login():
 
+    from core.database import get_connection
+    import pandas as pd
+    import streamlit as st
+
+    conn = get_connection()
+
+    df = pd.read_sql(
+        "SELECT id, username, role, active FROM users",
+        conn
+    )
+
+    st.dataframe(df)
+
     st.sidebar.markdown(
         "## 🔐 Cuenta Myrux"
     )
